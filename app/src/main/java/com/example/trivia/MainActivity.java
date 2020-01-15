8e2ebc93aa4b494dd544884226a22bee94758eb8
Nesar Ahmed <nesar13@hotmail.com>
2020-01-14 19:15:57 -0500

    Initial commit

---
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.trivia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

c7a48810ffee1452e8d620b72775a46c5ef3142b
Nesar Ahmed <nesar13@hotmail.com>
2020-01-19 14:18:09 -0500

    Update QuestionBank.java

--- app/src/main/java/com/example/trivia/data/QuestionBank.java
+++ app/src/main/java/com/example/trivia/data/QuestionBank.java
@@ -1,4 +1,35 @@
 package com.example.trivia.data;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.example.trivia.model.Question;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuestionBank {
+
+    ArrayList<Question> questionArrayList = new ArrayList<>();
+    private String url = "https://raw.githubusercontent.com/curiousily/simple-quiz/master/script/statements-data.json";
+
+    public List<Question> getQuestions() {
+        JsonArrayRequest jsonArrayRequest= new JsonArrayRequest(
+                Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }
+        );
+        return null;
+    }
 }

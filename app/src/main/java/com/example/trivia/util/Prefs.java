33e603f98cd917cddbb920aa68738608d14280f7
Nesar Ahmed <nesar13@hotmail.com>
2020-02-05 18:32:51 -0500

    Creating preferences to retrieve highest score

---
+++ app/src/main/java/com/example/trivia/util/Prefs.java
@@ -0,0 +1,28 @@
+package com.example.trivia.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class Prefs {
+    private SharedPreferences preferences;
+
+    public void Prefs(Activity activity) {
+        this.preferences = activity.getPreferences(activity.MODE_PRIVATE);
+
+    }
+
+    public void savedHighestScore(int score){
+        int currentScore=score;
+
+        int lastScore=preferences.getInt("high_score", 0);
+        if (currentScore >lastScore){
+
+            preferences.edit().putInt("high_score",currentScore).apply();
+
+
+
+
+        }
+    }
+}

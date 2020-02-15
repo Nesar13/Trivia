46599d924cbb73803d2436806f5e9441d68cdfcd
Nesar Ahmed <nesar13@hotmail.com>
2020-02-14 20:48:46 -0500

    Update answerListAsync.java

--- app/src/main/java/com/example/trivia/data/answerListAsync.java
+++ app/src/main/java/com/example/trivia/data/answerListAsync.java
@@ -4,6 +4,10 @@ import com.example.trivia.model.Question;
 
 import java.util.ArrayList;
 
+/**
+ * Just a callback class
+ *
+ */
 public interface answerListAsync {
 
     void processFinished(ArrayList<Question> question);
